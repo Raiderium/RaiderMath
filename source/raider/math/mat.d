5b30ef8415e972460556f2a4e846c9b19b1a158e
Aiden Sibley <raiderium.neo@gmail.com>
2014-12-08 00:45:18 +1000

    Minor optimisations

--- source/raider/math/mat.d
+++ source/raider/math/mat.d
@@ -23,150 +23,152 @@ struct Mat(int _D, _F) if(2 <= _D && _D <= 4 && isMatType!_F)
 	alias Vec!(D, F) Vec_;
 	alias Mat!(D, F) Mat_;
 
+	union
+	{
+		F f[D*D] = 0;
+		Vec_ v[D];
+	}
+
 	static if(D == 2)
 	{
-		Vec_ v[D] = [Vec_(1,0), Vec_(0,1)];
 		this(T=int)(Vec_ v0, Vec_ v1)
-		{
-			v[0] = v0;
-			v[1] = v1;
-		}
+		{ v[] = [v0, v1]; }
+
+		this(T=int)(F f0, F f1, F f2, F f3)
+		{ f[] = [f0, f1, f2, f3]; }
+
+		static Mat_ identity = Mat_(1,0,
+		                            0,1);
 	}
 
 	static if(D == 3)
 	{
-		Vec_ v[D] = [Vec_(1,0,0), Vec_(0,1,0), Vec_(0,0,1)];
 		this(T=int)(Vec_ v0, Vec_ v1, Vec_ v2)
-		{
-			v[0] = v0;
-			v[1] = v1;
-			v[2] = v2;
-		}
+		{ v[] = [v0, v1, v2]; }
+
+		this(T=int)(F f0, F f1, F f2, F f3, F f4, F f5, F f6, F f7, F f8)
+		{ f[] = [f0, f1, f2, f3, f4, f5, f6, f7, f8]; }
+		
+		static Mat_ identity = Mat_(1,0,0,
+		                            0,1,0,
+		                            0,0,1);
 	}
 
 	static if(D == 4)
 	{
-		Vec_ v[D] = [Vec_(1,0,0,0), Vec_(0,1,0,0), Vec_(0,0,1,0), Vec_(0,0,0,1)];
-		this(T=int)(Vec_ v0, Vec_ v1, Vec_ v2, Vec_ v3)
-		{
-			v[0] = v0;
-			v[1] = v1;
-			v[2] = v2;
-			v[3] = v3;
-		}
+		this(T=int)(Vec_ v0, Vec_ v1, Vec_ v2, Vec_ v3, Vec_ v4)
+		{ v[] = [v0, v1, v2, v3, v4]; }
 
-		//Construct affine rotation and translation matrix
-		this(T=int)(Mat!(3, T) o, Vec!(3, T) p)
+		this(T=int)(F f0,  F f1,  F f2,  F f3,  
+		            F f4,  F f5,  F f6,  F f7,  
+		            F f8,  F f9,  F f10, F f11,
+		            F f12, F f13, F f14, F f15)
+		{ f[] = [f0,  f1,  f2,  f3, 
+				 f4,  f5,  f6,  f7,
+				 f8,  f9,  f10, f11,
+				 f12, f13, f14, f15]; }
+		
+		static Mat_ identity = Mat_(1,0,0,0,
+		                            0,1,0,0,
+		                            0,0,1,0,
+		                            0,0,0,1);
+
+		//Construct affine orientation and translation matrix
+		this(T=int)(auto ref in Mat!(3, T) o, Vec!(3, T) p)
 		{
-			v[0][0..3] = o[0][]; v[0][3] = 0.0;
-			v[1][0..3] = o[1][]; v[1][3] = 0.0;
-			v[2][0..3] = o[2][]; v[2][3] = 0.0;
-			v[3][0..3] = p[]; 	 v[3][3] = 1.0;
+			f[0..3]   = o.f[0..3]; f[3]  = 0.0;
+			f[4..7]   = o.f[3..6]; f[7]  = 0.0;
+			f[8..11]  = o.f[6..9]; f[11] = 0.0;
+			f[12..15] =   p[0..3]; f[15] = 1.0;
 		}
 	}
 
 	//Construct submatrix, cutting out row r and column c.
 	static if(D < 4)
 	{
-		this(T=int)(Mat!(D+1, F) o, uint r, uint c)
+		this(T=int)(auto ref in Mat!(D+1, F) o, uint r, uint c)
 		{
 			assert(r < D);
 			assert(c < D);
 
-			for(uint ri=0; ri<D; ri++)
-			{
-				for(uint ci=0; ci<D; ci++)
-				{
+			foreach(ri; 0..D)
+				foreach(ci; 0..D)
 					v[ri][ci] = o[ri+(ri >= r)][ci+(ci >= c)];
-				}
-			}
 		}
 	}
 
 	this(T)(const T* o) if(isMatType!T)
 	{
-		for(uint x=0; x<D*D; x++)
-			ptr[x] = cast(F) o[x];
+		foreach(x; 0..D*D) f[x] = cast(F) o[x];
 	}
 
-	void identify()
+	void opAssign(T)(auto ref in Mat!(D, T) o)
 	{
-		for(uint x=0; x<D; x++)
-			for(uint y=0; y<D; y++)
-				v[x][y] = x==y ? 1.0 : 0.0;
-	}
-
-	/*
-	@property static Mat_ identity()
-	{
-		Mat_ m;
-		for(uint x=0; x<D; x++) m[x][x] = 1.0;
-		return m;
-	}*/
-
-	void opAssign(T)(Mat!(D, T) o)
-	{
-		for(uint x=0; x<D; x++) v[x] = o[x];
+		foreach(x; 0..D*D) f[x] = cast(F) o.f[x];
 	}
 
 	const Vec_ opIndex(const size_t i) { return v[i]; }
 	ref   Vec_ opIndex(const size_t i) { return v[i]; }
 
-	@property F* ptr() { return v[0].ptr; }
+	@property F* ptr() { return f.ptr; }
 
 	//matrix = this * matrix
-	Mat_ opBinary(string op)(Mat_ o) if(op == "*")
+	Mat_ opBinary(string op)(auto ref in Mat_ o) if(op == "*")
 	{
 		Mat!(D, T) result;
-		for(uint i=0; i<D; i++)
-			for(uint j=0; j<D; j++)
-				for(uint k=0; k<D; k++)
+		foreach(i; 0..D)
+			foreach(j; 0..D)
+				foreach(k; 0..D)
 					result[i][j] += v[i][k] * o[k][j];
+
 		return result;
 	}
 
 	//vec = this * vec
-	Vec_ opBinary(string op)(Vec_ o) if(op == "*")
+	Vec_ opBinary(string op)(auto ref in Vec_ o) if(op == "*")
 	{
 		Vec_ result;
-		for(uint r=0; r<D; r++)
-			for(uint c=0; c<D; c++)
+		foreach(r; 0..D)
+			foreach(c; 0..D)
 				result[r] += o[c]*v[r][c];
+
 		return result;
 	}
 
 	//matrix = this * scalar
 	Mat_ opBinary(string op)(F o) if(op == "*")
 	{
-		Mat_ result;
-		result.ptr[0..D*D] = ptr[0..D*D]*o;
-		return result;
+		Mat_ r = void;
+		r.f[0..D*D] = f[0..D*D]*o;
+
+		return r;
 	}
 
 	Mat_ transposed()
 	{
-		Mat_ t;
-		for(uint r=0; r<D; r++)
-			for(uint c=0; c<D; c++)
+		Mat_ t = void;
+		foreach(r; 0..D)
+			foreach(c; 0..D)
 				t[c][r] = v[r][c];
+
 		return t;
 	}
 
 	/**
 	 * Compute the adjoint matrix (that is, the transposed cofactor matrix).
 	 * 
-	 * Inefficient for 4x4.
+	 * Inefficient for 4x4 inversion. Use invert().
 	 */
 	Mat_ adjoint()
 	{
 		//There is no 1x1 submatrix constructor. Implement 2x2 adjoint directly.
-		static if(D == 2) return Mat_(Vec_(v[1][1], -v[1][0]), Vec_(v[0][1], -v[0][0]));
+		static if(D == 2) return Mat_(f[3], -f[2], f[1], -f[0]);
 		else
 		{
-			Mat_ adj;
+			Mat_ adj = void;
 
-			for(uint r=0; r<D; r++)
-				for(uint c=0; c<D; c++)
+			foreach(r; 0..D)
+				foreach(c; 0..D)
 					adj[c][r] = Mat!(D-1, F)(this, r, c).determinant * ((r+c)%2 ? -1 : 1);
 
 			return adj;
@@ -175,13 +177,14 @@ struct Mat(int _D, _F) if(2 <= _D && _D <= 4 && isMatType!_F)
 
 	F determinant()
 	{
-		static if(D == 2) return v[0][0]*v[1][1] - v[0][1]*v[1][0];
+		static if(D == 2) return f[0]*f[3] - f[1]*f[2];
 		else
 		{
-			F result = 0.0;
-			for(uint c=0; c<D; c++)
-				result += v[0][c] * Mat!(D-1, F)(this, 0, c).determinant * (c%2 ? -1 : 1);
-			return result;
+			F r = 0.0;
+			foreach(c; 0..D)
+				r += v[0][c] * Mat!(D-1, F)(this, 0, c).determinant * (c%2 ? -1 : 1);
+
+			return r;
 		}
 	}
 
@@ -192,11 +195,11 @@ struct Mat(int _D, _F) if(2 <= _D && _D <= 4 && isMatType!_F)
 			F det = determinant;
 			if(det == 0) return;
 
-			v[0][1] /= -det;
-			v[1][0] /= -det;
-			F temp = v[1][1]/det;
-			v[1][1] = v[0][0]/det;
-			v[0][0] = temp;
+			f[1] /= -det;
+			f[2] /= -det;
+			F temp = f[3]/det;
+			f[3] = f[0]/det;
+			f[0] = temp;
 		}
 
 		Mat_ rotation(F angle)
@@ -205,7 +208,7 @@ struct Mat(int _D, _F) if(2 <= _D && _D <= 4 && isMatType!_F)
 			F c = cos(angle);
 			F s = sin(angle);
 
-			return Mat_(Vec_(c, s), Vec_(-s, c));
+			return Mat_(c, s, -s, c);
 		}
 	}
 
@@ -214,42 +217,38 @@ struct Mat(int _D, _F) if(2 <= _D && _D <= 4 && isMatType!_F)
 		void invert()
 		{
 			//m stands for minor
-			F m0 = v[1][1]*v[2][2] - v[1][2]*v[2][1];
-			F m1 = v[2][2]*v[1][0] - v[1][2]*v[2][0];
-			F m2 = v[1][0]*v[2][1] - v[1][1]*v[2][0];
-			F m3 = v[0][1]*v[2][2] - v[0][2]*v[2][1];
-			F m4 = v[0][0]*v[2][2] - v[0][2]*v[2][0];
-			F m5 = v[0][0]*v[2][1] - v[0][1]*v[2][0];
-			F m6 = v[0][1]*v[1][2] - v[0][2]*v[1][1];
-			F m7 = v[0][0]*v[1][2] - v[0][2]*v[1][0];
-			F m8 = v[0][0]*v[1][1] - v[0][1]*v[1][0];
+			F m0 = f[4]*f[8] - f[5]*f[7];
+			F m1 = f[8]*f[3] - f[5]*f[6];
+			F m2 = f[3]*f[7] - f[4]*f[6];
+			F m3 = f[1]*f[8] - f[2]*f[7];
+			F m4 = f[0]*f[8] - f[2]*f[6];
+			F m5 = f[0]*f[7] - f[1]*f[6];
+			F m6 = f[1]*f[5] - f[2]*f[4];
+			F m7 = f[0]*f[5] - f[2]*f[3];
+			F m8 = f[0]*f[4] - f[1]*f[3];
 				 
 
 			F det = 
-				v[0][0] * m0 -
-				v[0][1] * m1 +
-				v[0][2] * m2;
+				f[0] * m0 -
+				f[1] * m1 +
+				f[2] * m2;
 
 			if(det == 0.0) return;
 
 			double invdet = 1.0/det;
 
-			Mat_ i;
-
 			//Notice this adds the sign to produce the cofactor matrix, 
 			//transposes to produce the adjoint matrix,
 			//and divides by the determinant to produce the inverse matrix.
-			i[0][0] =  m0 * invdet;
-			i[0][1] = -m3 * invdet;
-			i[0][2] =  m6 * invdet;
-			i[1][0] = -m1 * invdet;
-			i[1][1] =  m4 * invdet;
-			i[1][2] = -m7 * invdet;
-			i[2][0] =  m2 * invdet;
-			i[2][1] = -m5 * invdet;
-			i[2][2] =  m8 * invdet;
-
-			this = i;
+			f[0] =  m0 * invdet;
+			f[1] = -m3 * invdet;
+			f[2] =  m6 * invdet;
+			f[3] = -m1 * invdet;
+			f[4] =  m4 * invdet;
+			f[5] = -m7 * invdet;
+			f[6] =  m2 * invdet;
+			f[7] = -m5 * invdet;
+			f[8] =  m8 * invdet;
 		}
 
 		Mat_ rotation(double angle, int axis)
@@ -262,15 +261,15 @@ struct Mat(int _D, _F) if(2 <= _D && _D <= 4 && isMatType!_F)
 
 			switch(axis)
 			{
-				case 0: return Mat_(Vec_( 1, 0, 0), 
-					                Vec_( 0, c, s), 
-					                Vec_( 0,-s, c));
-				case 1: return Mat_(Vec_( c, 0,-s), 
-					                Vec_( 0, 1, 0), 
-					                Vec_( s, 0, c));
-				case 2: return Mat_(Vec_( c, s, 0), 
-					                Vec_(-s, c, 0), 
-					                Vec_( 0, 0, 1));
+				case 0: return Mat_(1, 0, 0, 
+					                0, c, s, 
+					                0,-s, c);
+				case 1: return Mat_(c, 0,-s, 
+					                0, 1, 0, 
+					                s, 0, c);
+				case 2: return Mat_(c, s, 0, 
+					               -s, c, 0, 
+					                0, 0, 1);
 				default: assert(0);
 			}
 		}
@@ -305,20 +304,20 @@ struct Mat(int _D, _F) if(2 <= _D && _D <= 4 && isMatType!_F)
 			 */
 
 			//s stands for submatrix
-			F s0 = v[0][0] * v[1][1] - v[1][0] * v[0][1];
-			F s1 = v[0][0] * v[1][2] - v[1][0] * v[0][2];
-			F s2 = v[0][0] * v[1][3] - v[1][0] * v[0][3];
-			F s3 = v[0][1] * v[1][2] - v[1][1] * v[0][2];
-			F s4 = v[0][1] * v[1][3] - v[1][1] * v[0][3];
-			F s5 = v[0][2] * v[1][3] - v[1][2] * v[0][3];
+			F s0 = f[0] * f[5] - f[4] * f[1];
+			F s1 = f[0] * f[6] - f[4] * f[2];
+			F s2 = f[0] * f[7] - f[4] * f[3];
+			F s3 = f[1] * f[6] - f[5] * f[2];
+			F s4 = f[1] * f[7] - f[5] * f[3];
+			F s5 = f[2] * f[7] - f[6] * f[3];
 
 			//c stands for complementary submatrix
-			F c5 = v[2][2] * v[3][3] - v[3][2] * v[2][3];
-			F c4 = v[2][1] * v[3][3] - v[3][1] * v[2][3];
-			F c3 = v[2][1] * v[3][2] - v[3][1] * v[2][2];
-			F c2 = v[2][0] * v[3][3] - v[3][0] * v[2][3];
-			F c1 = v[2][0] * v[3][2] - v[3][0] * v[2][2];
-			F c0 = v[2][0] * v[3][1] - v[3][0] * v[2][1];
+			F c5 = f[10] * f[15] - f[14] * f[11];
+			F c4 = f[9]  * f[15] - f[13] * f[11];
+			F c3 = f[9]  * f[14] - f[13] * f[10];
+			F c2 = f[8]  * f[15] - f[12] * f[11];
+			F c1 = f[8]  * f[14] - f[12] * f[10];
+			F c0 = f[8]  * f[13] - f[12] * f[9];
 			
 			F det =
 					s0 * c5 -
@@ -332,27 +331,27 @@ struct Mat(int _D, _F) if(2 <= _D && _D <= 4 && isMatType!_F)
 			
 			F invdet = 1.0/det;
 			
-			Mat_ i;
+			Mat_ i = void;
+
+			i.f[0]  = ( f[5]  * c5 - f[6]  * c4 + f[7]  * c3) * invdet;
+			i.f[1]  = (-f[1]  * c5 + f[2]  * c4 - f[3]  * c3) * invdet;
+			i.f[2]  = ( f[13] * s5 - f[14] * s4 + f[15] * s3) * invdet;
+			i.f[3]  = (-f[9]  * s5 + f[10] * s4 - f[11] * s3) * invdet;
 			
-			i[0][0] = ( v[1][1] * c5 - v[1][2] * c4 + v[1][3] * c3) * invdet;
-			i[0][1] = (-v[0][1] * c5 + v[0][2] * c4 - v[0][3] * c3) * invdet;
-			i[0][2] = ( v[3][1] * s5 - v[3][2] * s4 + v[3][3] * s3) * invdet;
-			i[0][3] = (-v[2][1] * s5 + v[2][2] * s4 - v[2][3] * s3) * invdet;
+			i.f[4]  = (-f[4]  * c5 + f[6]  * c2 - f[7]  * c1) * invdet;
+			i.f[5]  = ( f[0]  * c5 - f[2]  * c2 + f[3]  * c1) * invdet;
+			i.f[6]  = (-f[12] * s5 + f[14] * s2 - f[15] * s1) * invdet;
+			i.f[7]  = ( f[8]  * s5 - f[10] * s2 + f[11] * s1) * invdet;
 			
-			i[1][0] = (-v[1][0] * c5 + v[1][2] * c2 - v[1][3] * c1) * invdet;
-			i[1][1] = ( v[0][0] * c5 - v[0][2] * c2 + v[0][3] * c1) * invdet;
-			i[1][2] = (-v[3][0] * s5 + v[3][2] * s2 - v[3][3] * s1) * invdet;
-			i[1][3] = ( v[2][0] * s5 - v[2][2] * s2 + v[2][3] * s1) * invdet;
-			
-			i[2][0] = ( v[1][0] * c4 - v[1][1] * c2 + v[1][3] * c0) * invdet;
-			i[2][1] = (-v[0][0] * c4 + v[0][1] * c2 - v[0][3] * c0) * invdet;
-			i[2][2] = ( v[3][0] * s4 - v[3][1] * s2 + v[3][3] * s0) * invdet;
-			i[2][3] = (-v[2][0] * s4 + v[2][1] * s2 - v[2][3] * s0) * invdet;
-			
-			i[3][0] = (-v[1][0] * c3 + v[1][1] * c1 - v[1][2] * c0) * invdet;
-			i[3][1] = ( v[0][0] * c3 - v[0][1] * c1 + v[0][2] * c0) * invdet;
-			i[3][2] = (-v[3][0] * s3 + v[3][1] * s1 - v[3][2] * s0) * invdet;
-			i[3][3] = ( v[2][0] * s3 - v[2][1] * s1 + v[2][2] * s0) * invdet;
+			i.f[8]  = ( f[4]  * c4 - f[5]  * c2 + f[7]  * c0) * invdet;
+			i.f[9]  = (-f[0]  * c4 + f[1]  * c2 - f[3]  * c0) * invdet;
+			i.f[10] = ( f[12] * s4 - f[13] * s2 + f[15] * s0) * invdet;
+			i.f[11] = (-f[8]  * s4 + f[9]  * s2 - f[11] * s0) * invdet;
+
+			i.f[12] = (-f[4]  * c3 + f[5]  * c1 - f[6]  * c0) * invdet;
+			i.f[13] = ( f[0]  * c3 - f[1]  * c1 + f[2]  * c0) * invdet;
+			i.f[14] = (-f[12] * s3 + f[13] * s1 - f[14] * s0) * invdet;
+			i.f[15] = ( f[8]  * s3 - f[9]  * s1 + f[10] * s0) * invdet;
 			
 			this = i;
 		}
@@ -361,10 +360,10 @@ struct Mat(int _D, _F) if(2 <= _D && _D <= 4 && isMatType!_F)
 		{
 			//Invert rotation and scale
 			Mat!(3, F) r = Mat!(3, F)(this, 3, 3);
-			r.invert();
+			r.invert;
 
 			//Invert translation
-			Vec!(3, F) t = Vec!(3, F)(-v[3][0], -v[3][1], -v[3][2]);
+			Vec!(3, F) t = Vec!(3, F)(-f[12], -f[13], -f[14]);
 			t = t * r;
 
 			this = Mat_(r, t);
